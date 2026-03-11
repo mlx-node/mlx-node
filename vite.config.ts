418d7a9c0e1203e636049272558103066d2b9467
LongYinan <lynweklm@gmail.com>
2026-03-11 18:01:02 +0800

    chore: upgrade deps

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,12 @@ import { fileURLToPath } from 'node:url';
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
+  lint: {
+    options: {
+      typeAware: true,
+      typeCheck: true,
+    },
+  },
   fmt: {
     printWidth: 120,
     tabWidth: 2,
